c621eeae10ac0be5cb78fb0a65f5203ea2cbae96
Gusttab <Gusttab@DESKTOP-7MJE4A4.bbrouter>
2024-06-07 01:38:46 +0200

    Primero

--- src/entidades/vehiculo.java
+++ src/entidades/vehiculo.java
@@ -3,9 +3,55 @@ package entidades;
 
 public class vehiculo {
     //Parametros aplicando el encapulamiento
-    private String tipoVehiculo;
+    private String marca;
+    private String modelo;
+    private int año;
+    private String tipo;
     
-    //Contructor
-    public vehiculo(){
+    //contructor por defecto
+    public vehiculo(){}
+    
+    //Contructor Parametrizado
+    public vehiculo(String marca, String modelo, int año, String tipo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.tipo = tipo;
+    }
+    
+    //Metodos Getter
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    
+    //Metodo setter
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+    
+    //Metodo Setter
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    
+    //Metodo Setter
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 }
