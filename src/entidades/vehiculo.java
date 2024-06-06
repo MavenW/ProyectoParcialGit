3d10382e8f1c47b03063bcd08dd91665df084b7e
Gusttab <Gusttab@DESKTOP-7MJE4A4.bbrouter>
2024-06-07 01:19:45 +0200

    Primero

--- src/entidades/vehiculo.java
+++ src/entidades/vehiculo.java
@@ -1,5 +1,11 @@
+//Clase realizada por Gustavo Garcia
 package entidades;
 
 public class vehiculo {
+    //Parametros aplicando el encapulamiento
     private String tipoVehiculo;
+    
+    //Contructor
+    public vehiculo(){
+    }
 }
