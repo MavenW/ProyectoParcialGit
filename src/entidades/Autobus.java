d68383a460f065929470113f5302cebaf5ab94b2
Gusttab <Gusttab@DESKTOP-7MJE4A4.bbrouter>
2024-06-07 03:04:26 +0200

    reacion de la clase Hija Autobus

---
+++ src/entidades/Autobus.java
@@ -0,0 +1,36 @@
+//Clase Hija creada por Gustavo Garcia
+package entidades;
+
+//Clase hija que hereda de Vehiculo
+public class Autobus extends vehiculo {
+    //Atributos con encapsulamiento
+    private int capacidadPasajeros;
+    private boolean tieneBaño;
+    
+    //Contructor Parametrizado que hereda atributos de la clase padre
+    public Autobus(String marca, String modelo, int año, String tipo, int capacidadPasajeros, boolean tieneBaño) {
+        super(marca, modelo, año, tipo);
+        this.capacidadPasajeros = capacidadPasajeros;
+        this.tieneBaño = tieneBaño;
+    }
+    
+    //Metodo Getter para el atributo capacidadPasageros
+    public int getCapacidadPasajeros() {
+        return capacidadPasajeros;
+    }
+    
+    //Metodo Setter para el atributo capacidadPasageros
+    public void setCapacidadPasajeros(int capacidadPasajeros) {
+        this.capacidadPasajeros = capacidadPasajeros;
+    }
+    
+    //Metodo Getter para el atributo isTieneBaño
+    public boolean isTieneBaño() {
+        return tieneBaño;
+    }
+    
+    //Metodo Setter para el atributo Tienebaño
+    public void setTieneBaño(boolean tieneBaño) {
+        this.tieneBaño = tieneBaño;
+    }
+}
