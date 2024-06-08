47e208d54b181e37064d6486d23764cff2a2b416
ColdHart <ColdHart@DESKTOP-NGSJP18>
2024-06-08 01:09:16 -0500

    Creacion del metodo "Mecanico"

--- src/entidades/Mecanico.java
+++ src/entidades/Mecanico.java
@@ -8,6 +8,9 @@ public class Mecanico extends Persona{//inicio de la clase mecanico
     public Mecanico(String nombres, String apellidos, String cedula, int edad){
         super(nombres,apellidos,cedula,edad);
         
+    //metodo mostrar informacion (usamos los getters de la clase Persona)
+    
+        
     }
 
 }//fin de la clase mecanico
