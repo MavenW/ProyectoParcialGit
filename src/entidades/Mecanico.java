1be45ab94b1edb633fc9c36a585aa281148b3544
MavenW <159948883+MavenW@users.noreply.github.com>
2024-06-08 08:40:37 +0200

    Update

--- src/entidades/Mecanico.java
+++ src/entidades/Mecanico.java
@@ -8,6 +8,7 @@ public class Mecanico extends Persona{//inicio de la clase mecanico
     //constructor "Atribudos heredados de persona"
     public Mecanico(String nombres, String apellidos, String cedula, int edad){
         super(nombres,apellidos,cedula,edad);
+    }
         
     //metodo mostrar informacion (usamos los getters de la clase Persona)
     public void mostrarInfo(){
